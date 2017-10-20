82d3eb40ea6cc99391dee7b012e80ef430640550
Chimnutz <christianchimner@gmail.com>
2017-10-20 18:33:41 -0400

    Added easy run class
    Added easy run class

---
+++ C#Code/WorkoutScheduleBuilder/RunTypes/EasyRun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutApp.RunTypes
+{
+    class EasyRun
+    {
+        public double mileage { get; set; }
+        public string pace { get; set; }
+
+    }
+}
