1e5d2fdbb4e21e37a2a1735882f71fc521277ad7
Chimnutz <christianchimner@gmail.com>
2017-11-06 11:41:56 -0500

    Working mileage table definiton

--- C#Code/WorkoutAppWPF/MainWindow.xaml.cs
+++ C#Code/WorkoutAppWPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,7 +13,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Text.RegularExpressions;
 
 namespace WorkoutAppWPF
 {
@@ -21,15 +21,186 @@ namespace WorkoutAppWPF
     /// </summary>
     public partial class MainWindowApp : Window
     {
+        Grid mileageGrid;
+        List<TextBox> mileageList;
+
         public MainWindowApp()
         {
             InitializeComponent();
+
+            setDefaults();
+
+            createMileageTable((int)double.Parse(numCyclesInput.Text));
+
+            TextBox thisTextBox = mileageList[0];
+
+
+        }
+
+        private void setDefaults()
+        {
+            numCyclesInput.TextChanged -= numCycles_Changed;
+
+            numCyclesInput.Text = 16.ToString();
+            numDaysInput.Text = 9.ToString();
+            numWeeksInput.Text = (Math.Round(double.Parse(numCyclesInput.Text) * double.Parse(numDaysInput.Text)/7*10)/10).ToString();
+            minMileageInput.Text = 10.ToString();
+            maxMileageInput.Text = 40.ToString();
+            cycleMileageIncrease.Text = 4.ToString();
+            numCyclesReset.Text = 4.ToString();
+            cycleMileageDeltaInput.Text = 6.ToString();
+
+            numCyclesInput.TextChanged += numCycles_Changed;
+
+        }
+
+        private void recalcMileage_click(object sender, EventArgs e)
+        {
+            mileageDefStackPanel.Children.Remove(mileageGrid);
+            createMileageTable((int)double.Parse(numCyclesInput.Text));
+
+        }
+
+        private void numCycles_Changed(object sender, EventArgs e)
+        {
+            if (numCyclesInput.Text != "")
+            {
+                if (double.Parse(numCyclesInput.Text) > 0)
+                {
+                    numWeeksInput.Text = (Math.Round(double.Parse(numCyclesInput.Text) * double.Parse(numDaysInput.Text) / 7 * 10) / 10).ToString();
+                }
+            }
+            
         }
 
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^0-9]+");
             e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void createMileageTable(int rows)
+        {
+
+            mileageList = new List<TextBox>();
+
+            mileageGrid = new Grid();
+            mileageGrid.Width = 150;
+            mileageGrid.HorizontalAlignment = HorizontalAlignment.Left;
+            mileageGrid.VerticalAlignment = VerticalAlignment.Top;
+
+            // Create Columns
+            ColumnDefinition gridCol1 = new ColumnDefinition();
+            gridCol1.Width = new GridLength(2, GridUnitType.Star);
+            mileageGrid.ColumnDefinitions.Add(gridCol1);
+
+            ColumnDefinition gridCol2 = new ColumnDefinition();
+            gridCol2.Width = new GridLength(3, GridUnitType.Star);
+            mileageGrid.ColumnDefinitions.Add(gridCol2);
+
+
+            int numberOfCycles = (int)Math.Ceiling(double.Parse(numCyclesInput.Text));
+
+            // Create Rows
+            for (int ii = 0; ii < numberOfCycles+1; ii++)
+            {
+                RowDefinition gridRow = new RowDefinition();
+                mileageGrid.RowDefinitions.Add(gridRow);
+            }
+
+            // Add first column header
+            Label headerLabel1 = new Label();
+            headerLabel1.Margin = new Thickness(0, 10, 0, 0);
+            headerLabel1.Content = "Cycle";
+            headerLabel1.FontWeight = FontWeights.Bold;
+            headerLabel1.VerticalAlignment = VerticalAlignment.Top;
+            Grid.SetRow(headerLabel1, 0);
+            Grid.SetColumn(headerLabel1, 0);
+            mileageGrid.Children.Add(headerLabel1);
+
+            Label headerLabel2 = new Label();
+            headerLabel2.Margin = new Thickness(0, 10, 0, 0);
+            headerLabel2.Content = "Mileage";
+            headerLabel2.FontWeight = FontWeights.Bold;
+            headerLabel2.VerticalAlignment = VerticalAlignment.Top;
+            Grid.SetRow(headerLabel2, 0);
+            Grid.SetColumn(headerLabel2, 1);
+            mileageGrid.Children.Add(headerLabel2);
+
+            //build the table with the contols
+            double mileageDelta = double.Parse(cycleMileageIncrease.Text);
+            double doubleCycleMileage = 0;
+            double maxMileage = double.Parse(maxMileageInput.Text);
+            Boolean mileageExceeded = false;
+            double lastMileageTgt = double.Parse(minMileageInput.Text);
+            int numCyclesRest = (int)Math.Ceiling(double.Parse(numCyclesReset.Text));
+            int periodCnt = numCyclesRest;
+
+            for (int ii = 0; ii < numberOfCycles; ii++)
+            {
+
+                if (ii == 0)
+                {
+                    doubleCycleMileage = double.Parse(minMileageInput.Text);
+
+                }
+                else
+                {
+                    if (mileageExceeded == false)
+                    {
+                        doubleCycleMileage = lastMileageTgt + mileageDelta;
+
+                        if (doubleCycleMileage >= maxMileage)
+                        {
+                            doubleCycleMileage = maxMileage;
+                            mileageExceeded = true;
+                        }
+                    }
+
+                    else
+                    {
+                        periodCnt++;
+                        if(periodCnt> numCyclesRest)
+                        {
+                            doubleCycleMileage = doubleCycleMileage - double.Parse(cycleMileageDeltaInput.Text);
+                            periodCnt = 1;
+                        }else
+                        {
+                            doubleCycleMileage = lastMileageTgt + double.Parse(cycleMileageDeltaInput.Text)/ (numCyclesRest-1);
+                            if (doubleCycleMileage >= maxMileage)
+                            {
+                                doubleCycleMileage = maxMileage;
+                                mileageExceeded = true;
+                            }
+                        }
+                    }
+
+
+                }
+
+                lastMileageTgt = doubleCycleMileage;
+
+                //Create the controls
+                Label cycleText = new Label();
+                cycleText.Content = (ii+1).ToString();
+                cycleText.Margin = new Thickness(5, 0, 0, 0);
+                Grid.SetRow(cycleText, ii+1);
+                Grid.SetColumn(cycleText, 0);
+                mileageGrid.Children.Add(cycleText);
+
+                TextBox mileageText = new TextBox();
+                mileageText.PreviewTextInput += NumberValidationTextBox;
+                mileageList.Add(mileageText);
+                mileageText.Text = (doubleCycleMileage).ToString();
+                mileageText.Margin = new Thickness(5, 0, 0, 0);
+                Grid.SetRow(mileageText, ii+1);
+                Grid.SetColumn(mileageText, 1);
+                mileageGrid.Children.Add(mileageText);
+            }
+
+            //add this control to the grid 
+            mileageDefStackPanel.Children.Add(mileageGrid);
+
         }
 
 
